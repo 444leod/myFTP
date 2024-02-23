63b35e287caeff83ffb5b35b95af3778cb1e7d69
Léo DUMONT <leo.dumont@epitech.eu>
2024-02-23 11:18:07 +0100

    fix(reply_code): fixed compilation error & moved serverMessages array in file instead of header.

--- src/network/reply_code.c
+++ src/network/reply_code.c
@@ -6,22 +6,55 @@
 */
 
 #include "ftp.h"
+#include "fcntl.h"
+
+const server_message_s serverMessages[] = {
+    {SERVICE_READY,
+        "Service ready in %d minutes."},
+    {DATA_CONNECTION_ALREADY_OPEN,
+        "Data connection already open; transfer starting."},
+    {FILE_STATUS_OK,
+        "File status okay; about to open data connection."},
+    {COMMAND_OK,
+        "Command okay."},
+    {HELP_MESSAGE,
+        "Help message."},
+    {SERVICE_READY_NEW_USER,
+        "Service ready for new user."},
+    {SERVICE_CLOSING_CONTROL_CONNECTION,
+        "Service closing control connection."},
+    {CLOSING_DATA_CONNECTION,
+        "Closing data connection."},
+    {ENTERING_PASSIVE_MODE,
+        "Entering Passive Mode (h1,h2,h3,h4,p1,p2)."},
+    {USER_LOGGED_IN,
+        "User logged in, proceed."},
+    {REQUESTED_FILE_ACTION_COMPLETED,
+        "Requested file action okay, completed."},
+    {PATHNAME_CREATED,
+        "Pathname created."},
+    {USERNAME_OK_NEED_PASSWORD,
+        "Username okay, need password."},
+    {NEED_ACCOUNT_FOR_LOGIN,
+        "Need account for login."},
+    {-1, NULL}
+};
 
 void display_message(int code, int socketFd)
 {
     switch (code) {
     case SERVICE_READY:
-        fprintf(socketFd, serverMessages[0].message, 0);
+        dprintf(socketFd, serverMessages[0].message, 0);
         return;
     case HELP_MESSAGE:
-        fprintf(socketFd, serverMessages[4].message);
+        dprintf(socketFd, serverMessages[4].message);
         return;
     default:
         break;
     }
     for (int i = 0; serverMessages[i].code != -1; i++) {
         if (serverMessages[i].code == code) {
-            fprintf(socketFd, serverMessages[i].message);
+            dprintf(socketFd, serverMessages[i].message);
             return;
         }
     }

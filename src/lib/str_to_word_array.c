385869c9ebf4a7e7b592c0f0d0958d0eaa57321c
Léo DUMONT <leo.dumont@epitech.eu>
2024-03-06 00:57:29 +0100

    feat(str to word array): added str to word array implmentation.

---
+++ src/lib/str_to_word_array.c
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2024
+** myFTP
+** File description:
+** str_to_word_array
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include "lib.h"
+
+char **str_to_word_array(char *str, char *delim)
+{
+    char **array = malloc(sizeof(char *));
+    int i = 0;
+    char *start = str;
+    char *end;
+
+    while ((end = strpbrk(start, delim)) != NULL) {
+        array[i] = my_strndup(start, end - start);
+        i++;
+        array = realloc(array, sizeof(char *) * (i + 1));
+        start = end + 1;
+    }
+    array[i] = my_strdup(start);
+    i++;
+    array = realloc(array, sizeof(char *) * (i + 1));
+    array[i] = NULL;
+    return array;
+}

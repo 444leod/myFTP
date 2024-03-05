09de5975f20dc0c8d036f6c7b1f4a883859a04b7
Léo DUMONT <leo.dumont@epitech.eu>
2024-03-06 00:52:44 +0100

    fix(unused functions): removed unused functions from source file.

--- src/handle_commands.c
+++ src/handle_commands.c
@@ -25,40 +25,8 @@ static void execute_command(char **command, client_t client, fd_set *readfds)
     commands[i].func(client, command, readfds);
 }
 
-//TO DELETE!!!!!!!!!
-typedef struct temp_s {
-    char special_char;
-    char *replace;
-}temp_s;
-
-//TO DELETE!!!!!!!!!
-temp_s special_chars[] = {
-    {'\r', "\\r"},
-    {'\n', "\\n"},
-    {'\t', "\\t"},
-    {'\0', NULL}
-};
-
-static char *replace_special_chars(char *str)
-{
-    //TO DELETE!!!!!!!!!
-    for (int i = 0; str[i]; i++) {
-        for (int j = 0; special_chars[j].replace; j++) {
-            if (str[i] == special_chars[j].special_char) {
-                char *tmp = str + i + 1;
-                char *tmp2 = str;
-                tmp2[i] = '\0';
-                str = supercat(3, tmp2, special_chars[j].replace, tmp);
-                break;
-            }
-        }
-    }
-    return str;
-}
-
 void handle_command(client_t client, fd_set *readfds)
 {
-    printf("Command: %s\n", replace_special_chars(client->command));
     char *command = my_strdup(client->command);
     char **args = str_to_word_array(command, " \t");
 

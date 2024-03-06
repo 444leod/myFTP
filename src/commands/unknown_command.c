f6095e444b900c8e6d37ee83d6c61bcec86accdf
Léo DUMONT <leo.dumont@epitech.eu>
2024-03-06 15:23:15 +0100

    fix(unknown commands): fix of error code for unknown command depending on current status

--- src/commands/unknown_command.c
+++ src/commands/unknown_command.c
@@ -6,12 +6,17 @@
 */
 
 #include "clientllist.h"
+#include "reply_code.h"
 #include <sys/select.h>
 
 void unknown_command(client_t client, char **args, fd_set *readfds)
 {
     (void)args;
     (void)readfds;
-    client->current_code = 500;
+    if (client->status != STATUS_LOGGED_IN) {
+        client->current_code = NOT_LOGGED_IN;
+    } else {
+        client->current_code = SYNTAX_ERROR;
+    }
     client->data_status = WRITING;
 }

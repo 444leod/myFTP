c91a7b1d14236badaf729493205f6c281ea814a9
Léo DUMONT <leo.dumont@epitech.eu>
2024-03-07 13:40:13 +0100

    feat(CWD): added CWD command implementation

---
+++ src/commands/cwd.c
@@ -0,0 +1,69 @@
+/*
+** EPITECH PROJECT, 2024
+** myFTP
+** File description:
+** cwd
+*/
+
+#include "ftp.h"
+#include "clientllist.h"
+#include "reply_code.h"
+#include "lib.h"
+#include <sys/select.h>
+#include <unistd.h>
+
+static void verify_path(client_t client, char *server_path)
+{
+    char *new_path = get_current_dir();
+
+    if (new_path == NULL) {
+        client->current_code = FILE_UNAVAILABLE;
+        return;
+    }
+    if (strncmp(new_path, server_path, strlen(server_path)) != 0){
+        client->current_code = FILE_UNAVAILABLE;
+        return;
+    }
+    client->current_code = REQUESTED_FILE_ACTION_COMPLETED;
+    client->pwd = supercat(2, new_path + strlen(server_path), "/");
+}
+
+static void change_working_directory(client_t client, char *path, server_info_t server_info)
+{
+    char *new_path = NULL;
+
+    if (client->status != STATUS_LOGGED_IN) {
+        client->current_code = NOT_LOGGED_IN;
+        return;
+    }
+    if (strlen(path) == 0) {
+        client->current_code = FILE_UNAVAILABLE;
+        return;
+    }
+    new_path = supercat(3, server_info->path, client->pwd, path);
+    if (chdir(new_path) == -1) {
+        client->current_code = FILE_UNAVAILABLE;
+        return;
+    }
+    verify_path(client, server_info->path);
+}
+
+void cwd(client_t client, char **args,
+    fd_set *readfds, server_info_t server_info)
+{
+    int len = tablen((void **)args);
+    char *path = NULL;
+
+    (void)readfds;
+    (void)server_info;
+    switch (len) {
+        case 2:
+            path = args[1][0] == '/' ? args[1] + 1 : args[1];
+            change_working_directory(client, path, server_info);
+            break;
+        default:
+            client->current_code = SYNTAX_ERROR_IN_PARAMETERS;
+            break;
+    }
+    chdir(server_info->path);
+}

b9a71990691aaa750b96acdc5ce7c1f31c290eb9
Léo DUMONT <leo.dumont@epitech.eu>
2024-03-10 20:04:16 +0100

    hotfix(FTP-14): updated garbage collection with old lib for better freeing.

--- include/garbage_collector.h
+++ include/garbage_collector.h
@@ -1,20 +1,29 @@
 /*
-** EPITECH PROJECT, 2024
-** myFTP
+** EPITECH PROJECT, 2023
+** PROJETS
 ** File description:
-** garbage_collector
+** garbage
 */
+    #pragma once
 
-#pragma once
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <stdarg.h>
+    #include <unistd.h>
+    #include <stdint.h>
 
-#include <stdlib.h>
-#include <stdio.h>
+struct garbagec_t {
+    void *data;
+    struct garbagec_t *next;
+    struct garbagec_t *prev;
+};
 
-typedef struct garbage_node_s {
-    void *ptr;
-    struct garbage_node_s *next;
-} *garbage_node_t;
-
+typedef struct garbagec_t garbage_t;
+typedef garbage_t *g_llist_t;
+g_llist_t g_create(void *data);
+g_llist_t g_delete_begin(g_llist_t list);
+g_llist_t g_insert_end(void *data, g_llist_t list);
+g_llist_t *get_llist(void);
+void my_free_all(void);
+void my_free(void *pointer);
 void *my_malloc(size_t size);
-void my_free(void *ptr);
-void clear_garbage_collector(void);
--- src/lib/str_to_word_array.c
+++ src/lib/str_to_word_array.c
@@ -8,10 +8,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lib.h"
+#include "garbage_collector.h"
+
+static size_t count_words(char *str, char *delim)
+{
+    size_t count = 0;
+    char *start = str;
+    char *end;
+
+    end = strpbrk(start, delim);
+    while (end != NULL) {
+        count++;
+        start = end + 1;
+        end = strpbrk(start, delim);
+    }
+    count++;
+    return count;
+}
 
 char **str_to_word_array(char *str, char *delim)
 {
-    char **array = malloc(sizeof(char *));
+    char **array = my_malloc(sizeof(char *) * (count_words(str, delim) + 1));
     int i = 0;
     char *start = str;
     char *end;
@@ -20,13 +37,11 @@ char **str_to_word_array(char *str, char *delim)
     while (end != NULL) {
         array[i] = my_strndup(start, end - start);
         i++;
-        array = realloc(array, sizeof(char *) * (i + 1));
         start = end + 1;
         end = strpbrk(start, delim);
     }
     array[i] = my_strdup(start);
     i++;
-    array = realloc(array, sizeof(char *) * (i + 1));
     array[i] = NULL;
     return array;
 }
